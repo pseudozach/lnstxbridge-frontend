3b7e369a2b196e68588e5a4ef7af32cfd859d48a
pseudozach <zozsoy@gmail.com>
2021-12-10 08:40:09 +0000

    prepare

--- src/views/swap/steps/sendTransaction.js
+++ src/views/swap/steps/sendTransaction.js
@@ -47,7 +47,7 @@ import {
 import bigInt from 'big-integer';
 import { BN } from 'bn.js';
 
-let mocknet = new StacksMocknet();
+let mocknet = new StacksMocknet({url: 'https://3999-kumquat-boar-xv11ysj9.ws-us23.gitpod.io'});
 // mocknet.coreApiUrl = 'http://localhost:3999';
 const testnet = new StacksTestnet();
 const mainnet = new StacksMainnet();
