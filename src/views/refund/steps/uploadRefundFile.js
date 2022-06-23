6ee324743b645179f41278786bfc26e9cad446fb
pseudozach <git@pseudozach.com>
2022-06-23 16:12:01 -0700

    successful stx refund from continue page refactor uploadrefundfile view use refundlocal function

--- src/views/refund/steps/uploadRefundFile.js
+++ src/views/refund/steps/uploadRefundFile.js
@@ -58,55 +58,86 @@ const UploadRefundFileStyles = theme => ({
   },
 });
 
-const StyledUploadRefundFile = ({
-  classes,
-  setRefundFile,
-  setTransactionHash,
-  isUploaded,
-  refundFile,
-  setRefundFromTx,
-  // refundStx,
-}) => (
-  <View className={classes.wrapper}>
-    {isUploaded ? (
-      <CheckCircle
-        size={240}
-        style={{ fontSize: 240 }}
-        className={classes.icon}
-        color="success"
-      />
-    ) : (
-      <>
-        <DropZone className={classes.dropZone} onFileRead={setRefundFile}>
-          <p className={classes.info}>Drag the refund.png here</p>
-          {/* <span className={classes.info}>or click to select file</span> */}
-          {/* <FileUpload
+class StyledUploadRefundFile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      refundSet: false,
+    };
+  }
+
+  setRefundFromLocal = async setRefundLocal => {
+    try {
+      // check if refunding from localstorage
+      if (!window.location.href.includes('?')) return;
+      const swapId = window.location.href.split('?swapId=')[1];
+      if (!localStorage['lnswaps_' + swapId]) return;
+      const swapData = localStorage['lnswaps_' + swapId];
+      console.log('setRefundFromLocal ', setRefundLocal, swapData);
+      setRefundLocal(swapData);
+      this.setState({ refundSet: true });
+    } catch (error) {
+      console.log('error getting swapId: ', error.message);
+    }
+  };
+
+  render() {
+    const {
+      classes,
+      setRefundFile,
+      setTransactionHash,
+      isUploaded,
+      refundFile,
+      setRefundFromTx,
+      setRefundLocal,
+      // refundStx,
+    } = this.props;
+
+    if (!this.state.refundSet) {
+      this.setRefundFromLocal(setRefundLocal);
+    }
+
+    return (
+      <View className={classes.wrapper}>
+        {isUploaded ? (
+          <CheckCircle
+            size={240}
+            style={{ fontSize: 240 }}
+            className={classes.icon}
+            color="success"
+          />
+        ) : (
+          <>
+            <DropZone className={classes.dropZone} onFileRead={setRefundFile}>
+              <p className={classes.info}>Drag the refund.png here</p>
+              {/* <span className={classes.info}>or click to select file</span> */}
+              {/* <FileUpload
             text={'Select file'}
             onFileRead={setRefundFile}
             acceptMimeType={'image/png'}
           /> */}
-          <label htmlFor="icon-button-file">
-            <Input
-              accept="image/*"
-              id="icon-button-file"
-              type="file"
-              // onChange={setRefundFile}
-              onChange={event => {
-                setRefundFile(event.target.files[0]);
-              }}
-              hidden
-              sx={{ display: 'none' }}
-            />
-            <Button
-              variant="contained"
-              color="primary"
-              aria-label="upload picture"
-              component="span"
-              endIcon={<Upload />}
-            >
-              Upload
-            </Button>
-            {/* <IconButton
+              <label htmlFor="icon-button-file">
+                <Input
+                  accept="image/*"
+                  id="icon-button-file"
+                  type="file"
+                  // onChange={setRefundFile}
+                  onChange={event => {
+                    setRefundFile(event.target.files[0]);
+                  }}
+                  hidden
+                  sx={{ display: 'none' }}
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  aria-label="upload picture"
+                  component="span"
+                  endIcon={<Upload />}
+                >
+                  Upload
+                </Button>
+                {/* <IconButton
               color="primary"
               aria-label="upload picture"
               component="span"
@@ -115,38 +146,40 @@ const StyledUploadRefundFile = ({
             >
               <Upload />
             </IconButton> */}
-          </label>
-        </DropZone>
-        <View className={classes.regular}>
-          <p className={`${classes.mobileInfo}`}>
-            (Optional) If you don't have the refund file, paste Stacks lockStx
-            transaction Id
-          </p>
-          <InputArea
-            height={100}
-            width={400}
-            onChange={setRefundFromTx}
-            placeholder={`EG: ${sampleStacksTxId}`}
-          />
-        </View>
-      </>
-    )}
-    {refundFile.currency === 'BTC' ? (
-      <View className={classes.regular}>
-        <p className={`${classes.info} ${classes.mobileInfo}`}>
-          Paste the tx id of the BTC lockup transaction
-        </p>
-        <InputArea
-          height={100}
-          width={400}
-          className={classes.notfullwidth}
-          onChange={setTransactionHash}
-          placeholder={`EG: ${lockupTransactionHash}`}
-        />
+              </label>
+            </DropZone>
+            <View className={classes.regular}>
+              <p className={`${classes.mobileInfo}`}>
+                (Optional) If you don't have the refund file, paste Stacks
+                lockStx transaction Id
+              </p>
+              <InputArea
+                height={100}
+                width={400}
+                onChange={setRefundFromTx}
+                placeholder={`EG: ${sampleStacksTxId}`}
+              />
+            </View>
+          </>
+        )}
+        {refundFile.currency === 'BTC' ? (
+          <View className={classes.regular}>
+            <p className={`${classes.info} ${classes.mobileInfo}`}>
+              Paste the tx id of the BTC lockup transaction
+            </p>
+            <InputArea
+              height={100}
+              width={400}
+              className={classes.notfullwidth}
+              onChange={setTransactionHash}
+              placeholder={`EG: ${lockupTransactionHash}`}
+            />
+          </View>
+        ) : null}
       </View>
-    ) : null}
-  </View>
-);
+    );
+  }
+}
 
 StyledUploadRefundFile.propTypes = {
   classes: PropTypes.object.isRequired,
